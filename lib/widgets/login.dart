ae762d86e5f10409a250f578d4b28a5d25512cc5
José Romualdo <joseromualdo@outlook.com>
2023-09-22 21:48:28 -0300

    melhorar tela de login - campos

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -12,15 +12,35 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      body: Center(
+      body: Padding(
+        padding:  const EdgeInsets.all(10.0),
+        child: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Telefone'),
-            Text('Senha')
+            TextField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: TextStyle(color: Colors.white, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText:"Telefone do usuário",
+                  labelStyle: TextStyle(color: Colors.white),
+                )
+            ),  //TextField
+            TextField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(color: Colors.white, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText:"Senha do usuário",
+                  labelStyle: TextStyle(color: Colors.white),
+                )
+            ),//TextField
           ],
         ),
+      ),
       ),
     );
   }
